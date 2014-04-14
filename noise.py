aaff386373d7f10cf1a5173ce8709320077cf464
low-sky <rosolowsky@gmail.com>
2014-04-14 16:48:01 -0600

    Expansion of functionality for noise object including initial estimates of shape parameters.  Also nice noise plotting.

--- noise.py
+++ noise.py
@@ -1,10 +1,10 @@
 # &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
-# Imports
+# Importx
 # &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats
+import scipy.stats as ss
 from cube import *
 import time
 
@@ -21,15 +21,29 @@ class Noise:
     # Contents
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    noise_scalar = None
-    noise_map = None
+    # This is a global "representative" noise distribution width as a scalar
+    scalar = None   
+
+    # This is a spatial map of the width parameter
+    spatial_norm = None
+
+    # This is the 1D spectral normalization constructed so
+    # that spectral_norm[channel] * map[x,y] = noise width at
+    # this point in cube
+
     spectral_norm = None
 
+    # Noise distribution comaptible with scipy
+    distribution = None
+    distribution_shape = None
+    
     data = None
     spec_axis = None
 
-    signal = None
-    backup = None
+    # Holds the signal to noise ratio.
+    # SNR[x,y,z] = Data[x,y,z] / (scalar*spectral_norm[z]*spatial_norm[x,y])
+    snr = None
+
     
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Initialize and infrastructure
@@ -37,24 +51,95 @@ class Noise:
 
     def __init__(
         self,
-        data
-        ):
+        data,
+        scalar=None,
+        spatial_norm = None,
+        spectral_norm = None):
         """
         Construct a new Noise object.
         """
         self.data = data        
-        self.spec_axis = data.spec_axis
+#        self.spec_axis = data.spec_axis
+        self.distribution = ss.norm
+        self.calculate_fit()
+        
+    def calculate_fit(self):
+        """
+        Fits the distribution of the data using the scipy.fit
+        functionality.  This uses the distribution that characterizes
+        the noise values.
+        """
+        self.distribution_shape = self.distribution.fit(self.data)
+        return
 
 
-    def set_spectral_axis(
-        self,
-        val=None
-        ):
+    def calculate_naive(self):
         """
-        Set the spectral axis.
+        Calculates the naive values for the scalar and norms under the
+        assumption that the standard deviation is a rigorous method.
         """
-        if val is not None:
-            self.spec_axis = val
+        self.scalar = self.data.std()
+        self.spatial_norm = self.data.std(axis=0)/self.scalar
+        self.spectral_norm = self.data.reshape((self.data.shape[0],
+                                                self.data.shape[1]*
+                                                self.data.shape[2])).\
+                                                std(axis=1)/self.scalar
+        return
+
+    def calculate_mad(self):
+        """
+        Calculates the naive values for the scalar and norms under the
+        assumption that the standard deviation is a rigorous method.
+        """
+        self.scalar = mad(self.data)
+        self.spatial_norm = mad(self.data,axis=0)/self.scalar
+        self.spectral_norm = mad(self.data.reshape((self.data.shape[0],
+                                                self.data.shape[1]*
+                                                self.data.shape[2])),axis=1)/self.scalar
+        return
+
+    
+    def cube_of_noise(self):
+        """
+        Generates a matched data set of pure noise with properties matching
+        the stated distribution.
+        """
+        return self.distribution.rvs(*self.distribution_shape,
+                                     size=self.data.shape)
+
+    def plot_noise(self):
+        """
+        Makes a plot of the data distribution and the estimated
+        parameters of the PDF.
+        """
+        try:
+            import matplotlib.pyplot as pl
+        except ImportError:
+            return
+        xmin = self.distribution.ppf(1e-5,*self.distribution_shape)
+        xmax = self.distribution.ppf(0.99999,*self.distribution_shape)
+        xsamples = np.linspace(xmin,xmax,100)
+        Nbins = np.min([int(np.sqrt(self.data.size)),100])
+        binwidth = (xmax-xmin)/Nbins
+        pl.xlim(xmin,xmax)
+        pl.hist(self.data.ravel(),bins = Nbins,log=True)
+        pl.plot(xsamples,binwidth*self.data.size*
+                self.distribution.pdf(xsamples,*self.distribution_shape),
+                linewidth=4,alpha=0.75)
+        pl.xlabel('Data Value')
+        pl.ylabel('Number of Points')
+                
+        
+
+#     def set_spectral_axis(
+#         self,
+#         val=None
+#         ):
+#         """
+#         Set the spectral axis.
+#         """
+#         if val is not None:
+#             self.spec_axis = val
 
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Generate a noise estimate
@@ -144,44 +229,21 @@ class Noise:
 # These may be of general use and so are not part of the noise
 # class. Instead they can be called piecemeal.
 
-# ------------------------------------------------------------
-# NOISE ESTIMATORS
-# ------------------------------------------------------------
-
-def est_noise_1d(
-    data,
-    method="ROBUST"):
-    """
-    Calculate a single noise estimate for a vector.
-    """        
-
-    # Use a robust estimator (sigma clipping + M.A.D.)
-    if method == "ROBUST":
-        sig_thresh = \
-            sig_n_outliers(len(data), 
-                           n_out=1.0, 
-                           pos_only=True)        
-        return sigma_rob(data, 
-                         iterations=1, 
-                         thresh=sig_thresh)
-
-    # Use the M.A.D. only.
-    if method == "MAD":
-        return mad(data)
-
-    # Default to the standard deviation
-    return numpy.std(data)
 
 # ------------------------------------------------------------
 # STASTICS HELPER PROCEDURES
 # ------------------------------------------------------------
 
-def mad(data, sigma=True):
+def mad(data, sigma=True,axis = None):
     """
-    Return the median absolute deviation.
+    Return the median absolute deviation.  Axis functionality adapted from https://github.com/keflavich/agpy/blob/master/agpy/mad.py
     """
-    med = np.median(data)
-    mad = np.median(np.abs(data - med))
+    if axis>0:
+        med = np.median(data.swapaxes(0,axis),axis=0)
+        mad = np.median(np.abs(data.swapaxes(0,axis) - med),axis=0)
+    else:
+        med = np.median(data,axis=axis)
+        mad = np.median(np.abs(data - med),axis=axis)
     if sigma==False:
         return mad
     else:
